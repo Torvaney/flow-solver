c18cc7a64c0177c29a224272122665d1e1420749
Torvaney <torvaney@protonmail.com>
2019-05-22 22:43:19 +0100

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to flow-solver
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ project.clj
@@ -0,0 +1,13 @@
+(defproject flow-solver "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.clojure/math.combinatorics "0.1.5"]
+                 [org.clojure/test.check "0.10.0-alpha4"]]
+
+  :main flow-solver.core
+
+  :repl-options {:init-ns flow-solver.core})
