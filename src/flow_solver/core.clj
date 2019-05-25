417d50bec330091aa30ead044294b8b7b83cc09c
Torvaney <torvaney@protonmail.com>
2019-05-25 13:28:58 +0100

    Save to resources
    This is a bit of a hack. Need to find The Right Way to do this.

--- src/flow_solver/core.clj
+++ src/flow_solver/core.clj
@@ -11,10 +11,10 @@
   [map-file & args]
   (-> map-file slurp edn/read-string
       graph/init-graph
-      (graph/draw {:save {:filename (str (io/resource "output/before.png"))
+      (graph/draw {:save {:filename (str (io/resource "output") "/before.png")
                           :format   :png}})
       flow-sat/graph->sat
       sat/solve-symbolic-formula
       flow-sat/sat->graph
-      (graph/draw {:save {:filename (str (io/resource "output/after.png"))
+      (graph/draw {:save {:filename (str (io/resource "output") "/after.png")
                           :format   :png}})))
