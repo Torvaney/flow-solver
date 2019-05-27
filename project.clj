a63bbef72caca5362db7b17f7d26bf1bfe4e7bc4
Torvaney <torvaney@protonmail.com>
2019-05-27 19:50:45 +0100

    Update test deps

--- project.clj
+++ project.clj
@@ -3,13 +3,14 @@
   :url "https://github.com/Torvaney/flow-solver"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  
+
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/math.combinatorics "0.1.5"]
-                 [org.clojure/test.check "0.10.0-alpha4"]
                  [rolling-stones "1.0.0"]
                  [ubergraph "0.5.3"]]
 
   :main flow-solver.core
+  :repl-options {:init-ns flow-solver.core}
 
-  :repl-options {:init-ns flow-solver.core})
+  :profiles
+  {:test {:dependencies [[org.clojure/test.check "0.10.0-alpha3"]]}})
