7b3bb88833447833a5fb0b7012c0e0e7baf2dc47
Torvaney <torvaney@protonmail.com>
2019-06-20 22:54:35 +0100

    Print output during solving

--- src/flow_solver/core.clj
+++ src/flow_solver/core.clj
@@ -7,20 +7,26 @@
             [ubergraph.core :as uber]))
 
 
-(defn solve
-  [init]
-  (->> init 
-       flow-sat/graph->sat 
-       sat/solve-symbolic-formula
-       flow-sat/sat->graph))
+(def solve
+  (comp flow-sat/sat->graph
+        sat/solve-symbolic-formula
+        flow-sat/graph->sat))
 
 
 (defn -main
   [map-file & args]
-  (let [init   (-> map-file slurp edn/read-string graph/init-graph)
-        solved (solve init)]
-    (do (graph/draw init   {:save {:filename (str (io/resource "output") "/before.png")
-                                   :format   :png}})
-        (graph/draw solved {:save {:filename (str (io/resource "output") "/after.png")
-                                   :format   :png}})
-        (println "Done!"))))
+  ;; There must be a more elegant way than this...?
+  (let [map-edn (-> map-file slurp edn/read-string)
+        init    (graph/init-graph map-edn)
+        solved  (future (solve init))]
+    (do 
+      (println "Attempting to solve a " (:dim map-edn) "-by-" (:dim map-edn) " puzzle...")
+      (println (if @solved "Solved!" "No solution found. Are you _sure_ you gave the right map?"))
+      (println "Attempting to save before/after images...")
+
+      (graph/draw init    {:save {:filename (str (io/resource "output") "/before.png")
+                                  :format   :png}})
+      (graph/draw @solved {:save {:filename (str (io/resource "output") "/after.png")
+                                  :format   :png}})
+
+      (println "Done!"))))
